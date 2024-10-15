d719f2027a34a503b8a0b1bf716392f42c0747ba
chalky66 <chalky66@gmail.com>
2024-10-15 07:28:51 +0200

    Delete all files

--- leafletmap.js
+++
@@ -1,8 +0,0 @@
-﻿export function load_map(longitude, latitude) {
-    var map = new maplibregl.Map({
-        container: 'map',
-        style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
-        center: [longitude, latitude], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
-}
